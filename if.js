5e86b2c41d74569abfa3faf677398433909161be
Daniela de Almeida Lima <danielalimaa77@gmail.com>
2022-10-25 20:10:00 -0300

    commit if and else

---
+++ if.js
@@ -0,0 +1,39 @@
+
+
+    // 1 semestre notas
+
+alert('primeiro\nSemestre')
+let nota1 = prompt("Insira sua primeira nota")
+
+let nota2 = prompt('Insira sua segunda nota')
+
+let nota3 = prompt('Insira sua terceira nota')
+
+let media = ((nota1 + nota2 + nota3) / 2)
+
+
+if (media >=5) {
+  alert("Voce passou! " +  media )
+} else if (media <5) {
+  alert("Voce não passou " + media)
+}
+
+
+   // 2 semestre notas
+alert('segundo semestre')
+let nota4 = prompt("Insira sua quarta nota")
+
+let nota5 = prompt('Insira sua quinta nota')
+
+let nota6 = prompt('Insira sua sexta nota')
+
+let Segundamedia = ((nota4 + nota5 + nota6) / 2)
+
+if (Segundamedia >=5) {
+  alert("Voce passou! " + Segundamedia)
+} else if (Segundamedia <5) {
+  alert("Voce não passou " + Segundamedia)
+}
+
+
+
